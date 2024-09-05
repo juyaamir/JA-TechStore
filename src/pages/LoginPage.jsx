c8c398027b34c01f5bce24dbb9e574d53ebb2953
Juya <juyaamirm@gmail.com>
2024-09-05 17:27:30 +0200

    rule added to  form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,33 @@
 import React, {useState} from 'react'
 import logo from '../assets/ja.jpg'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 const LoginPage = () => {
-  const [showEmail, setShowEmail] = useState(true) 
+  const navigate = useNavigate();
+  const [showEmail, setShowEmail] = useState(true);
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+  });
+const handleInput = (e) => {
+    const {name, value} = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    })
+};
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    setInput({
+      email: '',
+      password: '',
+    });
+    setShowEmail(!showEmail);
+    navigate('/');
+};
+const handleNavigate = () => {
+    navigate(-1);
+};
   return (
     <div className='w-80 '>
       <div>
@@ -14,26 +39,28 @@ const LoginPage = () => {
 
         </div>
         <div className='flex flex-col gap-4'>
-          <form className='flex flex-col gap-5 border border-gray-300  p-4 rounded-lg'>
+          <form className='flex flex-col gap-5 border border-gray-300  p-4 rounded-lg shadow-lg' onSubmit={handleSubmit}>
             <h1 className=''>Sign In</h1>
+            {!showEmail && <p>{input.email} <Link className='text-cyan-700 hover:text-green-500' 
+            onClick={handleNavigate}>Change</Link> </p> }
             {
               showEmail ? (
-                <div className='flex flex-col gap-2'>
+              <div className='flex flex-col gap-2'>
                 <label htmlFor="email" className='font-semibold'>Enter your Email:</label>
-              <input type="email" id='email' name='email' className='border border-gray-300 rounded-md p-1'
-              placeholder='Email '/>
-              <button type='button' className='bg-cyan-700 hover:bg-green-500 text-white p-1 w-full rounded-md'
-              onClick={() => setShowEmail(!showEmail)}
-              >Continue</button>
-              <p className='text-sm'>
-              By continuing, you agree to JA-TechStore's <Link to='/condition-of-use' className='text-cyan-600 underline hover:text-green-500'>Conditions of Use</Link> and <Link to='/condition-of-use' className='text-cyan-600 underline hover:text-green-500'>Privacy Notice.</Link>
+                <input type="email" id='email' name='email' value={input.email} className='border border-gray-300 rounded-md p-1'
+                placeholder='Email ' onChange={handleInput} required/>
+                <Link type='button' className='bg-cyan-700 hover:bg-green-500 text-white p-1 w-full rounded-md text-center'
+                onClick={() => setShowEmail(!showEmail)}
+                >Continue</Link>
+                <p className='text-sm'>
+                By continuing, you agree to JA-TechStore's <Link to='/condition-of-use' className='text-cyan-600 underline hover:text-green-500'>Conditions of Use</Link> and <Link to='/condition-of-use' className='text-cyan-600 underline hover:text-green-500'>Privacy Notice.</Link>
               </p>
               </div>
               ) : (
               <div className='flex flex-col gap-2'>
                 <label htmlFor="password" className='font-semibold'>Enter your password:</label>
-                <input type="password" id='password' name='password' className='border border-gray-300 rounded-md p-1'
-                placeholder='Password '/>
+                <input type="password" id='password' name='password' value={input.password} className='border border-gray-300 rounded-md p-1'
+                placeholder='Password ' onChange={handleInput} required/>
                 <button 
                 type='submit' className='bg-cyan-700 hover:bg-green-500 text-white p-1 w-full rounded-md'
                 onClick={() => setShowEmail(false)}>Submit
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,47 +2,72 @@ import React, {useState} from 'react'
 import logo from '../assets/ja.jpg'
 import { Link } from 'react-router-dom'
 const Signup = () => {
-  const [showEmail, setShowEmail] = useState(true) 
+  const [showEmail, setShowEmail] = useState(true);
+  const [input, setInput] = useState({
+    name: '',
+    email: '',
+    password: '',
+    rePassword: ''
+  });
+  const handleInput = (e) => {
+    const {name, value} = e.target;
+    setInput({
+      ...input,
+      [name]: value
+    })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    setInput({
+      name: '',
+      email: '',
+      password: '',
+      rePassword: '',
+    })
+  }
   return (
-    <div className='w-80 '>
-      <div>
-        <div >
+      <div className='w-80 '>
+        <div>
           <Link to='/' className='flex  items-center justify-center my-3'>
             <img src={logo} alt="logo" className="h-14 w-14 rounded-full" />
             <h3>JA-TechStore</h3>
           </Link>
-
         </div>
         <div className='flex flex-col gap-4'>
-          <form className='flex flex-col gap-5 border border-gray-300  p-4 rounded-lg'>
+          <form className='flex flex-col gap-5 border border-gray-300  p-4 rounded-lg shadow-lg' onSubmit={handleSubmit} >
             <h1 className=''>Sign In</h1>
               <div className='flex flex-col gap-2'>
-
                 <div className='flex flex-col gap-1'>
                   <label htmlFor="name">Your Name:</label>
-                  <input type="text" id='name' name='name' className='border border-gray-300 rounded-md p-1'
+                  <input type="text" id='name' name='name' value={input.name} className='border border-gray-300 rounded-md p-1'
+                  onChange={handleInput} required
                   placeholder='First and last name '/>
                 </div>
                 <div className='flex flex-col gap-1'>
                   <label htmlFor="email">Email:</label>
-                  <input type="email" id='email' name='email' className='border border-gray-300 rounded-md p-1'
+                  <input type="email" id='email' name='email' value={input.email} className='border border-gray-300 rounded-md p-1'
+                  onChange={handleInput} required
                   placeholder='Email'/>
                 </div>
                 <div className='flex flex-col gap-1'>
                   <label htmlFor="password">Password:</label>
-                  <input type="password" id='password' name='password' className='border border-gray-300 rounded-md p-1'
+                  <input type="password" id='password' name='password' value={input.password} className='border border-gray-300 rounded-md p-1'
+                  onChange={handleInput} required
                   placeholder='At lest 6 characters'/>
                 </div>
                 <div className='flex flex-col gap-1'>
                   <label htmlFor="rePassword">Re-enter password:</label>
-                  <input type="password" id='rePassword' name='rePassword' className='border border-gray-300 rounded-md p-1'
+                  <input type="password" id='rePassword' name='rePassword' value={input.rePassword} className='border border-gray-300 rounded-md p-1'
+                  onChange={handleInput} required
                   />
                 </div>
-                <button type='button' className='bg-cyan-700 hover:bg-green-500 text-white p-1 w-full rounded-md'
-                onClick={() => setShowEmail(!showEmail)}
-                >Register now</button>
+                <button type='Submit' className='bg-cyan-700 hover:bg-green-500 text-white p-1 w-full rounded-md'
+                  onClick={() => setShowEmail(!showEmail)}
+                  >Register now</button>
                 <p className='text-sm'>
-                By creating an account, you agree to JA-TechStore's <Link to='/condition-of-use' className='text-cyan-600 underline hover:text-green-500'>Conditions of Use</Link> and <Link to='/condition-of-use' className='text-cyan-600 underline hover:text-green-500'>Privacy Notice.</Link>
+                  By creating an account, you agree to JA-TechStore's <Link to='/condition-of-use' className='text-cyan-600 underline hover:text-green-500'>Conditions of Use</Link> and <Link to='/condition-of-use' className='text-cyan-600 underline hover:text-green-500'>Privacy Notice.</Link>
                 </p>
               </div>
           </form>
@@ -54,8 +79,8 @@ const Signup = () => {
             Sign in</Link>
           </div>
         </div>
+
       </div>
-    </div>
   )
 }
 
