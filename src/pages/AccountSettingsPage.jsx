1866a730159a32c7119f23a42466b30149f0af13
Juya <juyaamirm@gmail.com>
2024-09-10 23:36:31 +0200

    deploy

--- src/pages/AccountSettingsPage.jsx
+++ src/pages/AccountSettingsPage.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
 
 const AccountSettingsPage = () => {
+  const [editName, setEditName] = useState(true);
   return (
-    <div>AccountSettingsPage</div>
+    <div className='  border-red-500 border'>
+      <div className='p-2'>
+        <h1>Account Settings</h1>
+        <p>Control, protect, and secure your account</p>
+      </div>
+      <div className=' border border-gray-300 rounded-md my-2 main-container'>
+        <div className='account-settings-container'>
+          <div className=''>
+            <p className='font-bold'>Name</p>
+            <p>Juya Amir</p>
+          </div>
+          <Link className='account-settings-edit'>Edit</Link>
+        </div>
+        <div className='account-settings-container'>
+          <div className=''>
+            <p className='font-bold'>Email</p>
+            <p>amirjuya@gmail.com</p>
+          </div>
+          <Link className='account-settings-edit'>Edit</Link>
+        </div>
+        <div className='account-settings-container'>
+          <div className=''>
+            <p className='font-bold'>Password</p>
+            <p>******</p>
+          </div>
+          <Link className='account-settings-edit'>Edit</Link>
+        </div>
+
+      </div>
+
+      {
+        editName && (
+          <div className='bg-red-400 '>
+            <h1>Edit Name</h1>
+            <form>
+              <div className='flex flex-col gap-2'>
+                <div className='flex flex-col gap-1'>
+                  <label htmlFor="name">Your Name</label>
+                  <input type="text" id='name' name='name' className='border border-gray-300 rounded-md p-1'
+                    required
+                    placeholder='First and last name ' />
+                </div>
+                <div className='flex flex-col gap-1'>
+                  <button className='bg-cyan-900 text-white p-1 rounded-md'>Update</button>
+                  <button className='bg-cyan-900 text-white p-1 rounded-md'>Cancel</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import profile from '../assets/profile.jpeg'
+import { Link } from 'react-router-dom'
+import { FiEdit } from "react-icons/fi";
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className='flex gap-8'>
+      <div className='flex flex-col text-center w-44 justify-center items-center p-2'>
+        <img src={profile} alt="image" className='h-28 w-28 rounded-full ' />
+        <div className='flex gap-2 '>
+          <h4 className='font-bold'>Amir Muhammad Juya</h4>
+          <Link title='Change profile photo' className='text-xl hover:text-orange-400'> <FiEdit /></Link>
+        </div>
+        
+      </div>
+      <div className='border border-gray-400 p-2'>
+        <h1 className='font-bold'>Your Profile</h1>
+        <p>Your profile preferences help us personalize recommendations for you</p>
+      </div>
+    </div>
   )
 }
 
