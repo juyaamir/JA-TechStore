49e8f66e267aa9e93cbc7b3054db5cebae55f1b7
Juya <juyaamirm@gmail.com>
2024-10-12 01:11:46 +0200

    deploy

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ImageSlider from '../components/ImageSlider'
 import image1 from '../images/image-1.jpg'
 import image4 from '../images/image-4.jpg'
-import image6 from '../images/image-6.avif'
-import image7 from '../images/image-7.avif'
+import image6 from '../images/image-6.jpg'
+import image7 from '../images/image-7.jpg'
 import image8 from '../images/image-8.jpg'
 const images = [image1, image4, image6, image7, image8];
 const HomePage = () => {
